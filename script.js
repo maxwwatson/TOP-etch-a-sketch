982f434838a6f356b575cf91ab2fabd2a11f92a0
Max <max.w1@hotmail.com>
2023-12-26 23:01:01 +0000

    Finished Etch a sketch - draw, hover, erase, Etc
    Fuck me dead I see the good and bad things with how these exercises are
    laid out. Breaking things down into super small problems is fantastic
    for actually figuring out how to code bit by bit as you edge towards
    the solution, however having to refractor and change everything you
    did and change all the functions is a pain and a half.
    THAT BEING SAID - I learned a lot about how important a few different
    things are:
    - Micro-commits would have been good to do after each feature
    - Writing out how my algorithm and how functions loop through
    eachother in full pseudo code would have preventing having to really
    get myself up to speed when I have to go back.
    - More forethought before writing this all out would make it easier
    to think through it all.

--- script.js
+++ script.js
@@ -2,10 +2,7 @@
 let squaresPerSide = 16;
 let squaresTotal = squaresPerSide**2;
 let squareArray = []; 
-
-// setup button
-const chooseSquaresBtn = document.querySelector("#choose-square");
-chooseSquaresBtn.addEventListener('click', changeSquares);
+let currentColor = 'black';
 
 // Getting and sizing grid  
 const container = document.querySelector('#etch-container');
@@ -16,35 +13,110 @@ let squareWidth = containerWidth / squaresPerSide;
 let squareHeight = containerHeight / squaresPerSide;
 
 let squareStyle = `width: ${squareWidth}px;
-                     height: ${squareHeight}px;`
+                     height: ${squareHeight}px;
+                     opacity: 1;`
 
-createSquares(squaresTotal);
+
+// setup buttons
+const chooseSquaresBtn = document.querySelector("#choose-square");
+const chooseDrawBtn = document.querySelector("#draw");
+const chooseHoverBtn = document.querySelector("#hover");
+const chooseRandomBtn = document.querySelector("#colourRandom");
+const chooseBlackBtn = document.querySelector("#colourBlack");
+const chooseRedBtn = document.querySelector("#colourRed");
+const chooseBlueBtn = document.querySelector("#colourBlue");
+const chooseEraseBtn = document.querySelector("#erase");
+
+
+chooseSquaresBtn.addEventListener('click', changeSquares); // no params
+chooseDrawBtn.addEventListener('click', changeBrush);
+chooseDrawBtn.brush = "draw";
+chooseHoverBtn.addEventListener('click', changeBrush);
+chooseHoverBtn.brush = "hover";
+chooseRandomBtn.addEventListener('click', randColour);
+chooseBlackBtn.addEventListener('click', () => currentColor = `rgb(0,0,0)`);
+chooseRedBtn.addEventListener('click', () => currentColor = `rgb(255,0,0)`);
+chooseBlueBtn.addEventListener('click', () => currentColor = `rgb(0,0,255)`);
+chooseEraseBtn.addEventListener('click',changeBrush); 
+chooseEraseBtn.brush = "erase";
+
+
+// setup
+createSquares();
+// changeBrush('draw');
 
 function randColour(event) {
     let red, blue, green;
     red = Math.floor(Math.random() * 255);
     green = Math.floor(Math.random() * 255);
     blue = Math.floor(Math.random() * 255);
-    console.log(red);
-    event.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    currentColor = `rgb(${red},${green},${blue})`;
+    // event.target.style.backgroundColor = `rgba(${red},${green},${blue},${alpha})`;
 }
 
+// yet to be implemented 
+function chooseColour(event) {
+    console.log(event.target.style.opacity);
+    event.target.style.backgroundColor = currentColor;
+    if(event.target.style.opacity == 1) {
+        console.log(true);
+         event.target.style.opacity = 0;
+    }
+            // let opacity1 = (parseFloat(event.target.style.opacity) - 0.1).toFixed();
+            // let opacity = 0.5;
 
+    event.target.style.opacity = parseFloat(event.target.style.opacity) + 0.1;
+    console.log(event.target.style.opacity);
+}
+
+// for hover - full opacity
+function chooseColourHover(event) {
+    event.target.style.backgroundColor = currentColor;
+    event.target.style.opacity = '1';
+}
 
 function whiteColour(event) {
     event.target.style.backgroundColor = 'rgb(255,255,255)';
 }
 
+
 // create all div squares
-function createSquares(squares) {
+function createSquares() {
     for(let i = 0; i < squaresTotal; i++){
         squareArray[i] = document.createElement('div');
         squareArray[i].setAttribute("style", squareStyle);
-        squareArray[i].addEventListener('mouseover', randColour);
-        squareArray[i].addEventListener('mouseout', whiteColour);
+        squareArray[i].classList.toggle("squareClass");
         //squareArray[i].classList.toggle('hoverEffect');
        // squareArray[i].addEventListener('hover', changeColour);
         container.appendChild(squareArray[i]);
+    }
+}
+
+function changeBrush(event) {
+    let allBoxes = document.getElementsByClassName("squareClass");
+    for(let i = 0; i < allBoxes.length; i++) {
+        if(event.currentTarget.brush == 'draw')
+        {
+            allBoxes[i].removeEventListener('mouseover', chooseColourHover);
+            allBoxes[i].removeEventListener('mouseout', whiteColour);
+
+            allBoxes[i].addEventListener('mouseover', chooseColour);
+            allBoxes[i].addEventListener('mouseout', () => null);
+
+            //squareArray[i].addEventListener('mouseout', whiteColour);
+        }
+        else if(event.target.brush == 'hover')
+        {
+            allBoxes[i].removeEventListener('mouseover', chooseColour);
+            allBoxes[i].removeEventListener('mouseout', () => null);
+            allBoxes[i].addEventListener('mouseover', chooseColourHover);
+            allBoxes[i].addEventListener('mouseout', whiteColour);
+        }
+        else if(event.target.brush == 'erase')
+        {
+            allBoxes[i].style.backgroundColor = 'white';
+            allBoxes[i].style.opacity = '1';
+        }
     }
 }
 
